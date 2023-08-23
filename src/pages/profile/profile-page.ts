8add5c35db42dda1bc38aaf0abf390a6be70b1af
Alban Nocaj <alban_nocaj@mckinsey.com>
2023-08-23 12:08:56 +0200

    clearnip header after bottom-navigation

--- src/pages/profile/profile-page.ts
+++ src/pages/profile/profile-page.ts
@@ -26,31 +26,9 @@ export class ProfilePage extends LitElement {
   }
   render() {
     return html`
-      <app-header ?enableBack="${true}"></app-header>
+      <app-header ?enableBack="${false}" title="${'Profile'}"></app-header>
 
       <main>
-        <h2>Profile Page</h2>
-
-        <sl-card>
-          <h2>Did you know?</h2>
-
-          <p>
-            PWAs have access to many useful APIs in modern browsers! These APIs
-            have enabled many new types of apps that can be built as PWAs, such
-            as advanced graphics editing apps, games, apps that use machine
-            learning and more!
-          </p>
-
-          <p>
-            Check out
-            <a
-              href="https://docs.microsoft.com/en-us/microsoft-edge/progressive-web-apps-chromium/how-to/handle-files"
-              >these docs</a
-            >
-            to learn more about the advanced features that you can use in your
-            PWA
-          </p>
-        </sl-card>
         <sl-button @click="${this.handleLogout}">Logout</sl-button>
       </main>
       <bottom-navigation activeRoute="profile"></bottom-navigation>
