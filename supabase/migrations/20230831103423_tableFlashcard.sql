1f0077e899fa4de84a7129f6b5be122f24fe6751
Alban Nocaj <alban_nocaj@mckinsey.com>
2023-08-31 12:47:36 +0200

    fix typo

--- supabase/migrations/20230831103423_tableFlashcard.sql
+++ supabase/migrations/20230831103423_tableFlashcard.sql
@@ -1,6 +1,6 @@
 create table public.flashcard (
   id integer primary key generated always as identity,
-  topic_id integer REFERENCES public.topic(id);
+  topic_id integer REFERENCES public.topic(id),
   front text,
   back text
 );
